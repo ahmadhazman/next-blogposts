9d92f5523c6c152e0df9c03106b65773bd0c9a18
ahmadhazman <ahmd.hazman@gmail.com>
2023-06-08 19:15:01 +0800

    first commmit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	// reactStrictMode: true,;
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'raw.githubusercontent.com',
+				port: '',
+				pathname: '/ahmadhazman/test-blogpost/main/images/**',
+			},
+		],
+	},
+	typescript: {
+		// !! WARN !!
+		// Dangerously allow production builds to successfully complete even if
+		// your project has type errors.
+		// !! WARN !!
+		ignoreBuildErrors: true,
+	},
 }
 
 module.exports = nextConfig
